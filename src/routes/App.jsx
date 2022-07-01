29e05cf7ad0baf92a0a9f55855b410f871a06b60
Aleincob <alecaperez13@gmail.com>
2022-07-01 10:48:30 -0500

    [ADD] GitHub Actions

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
     const initialState = useInitialState();
     return (
         <AppContext.Provider value={initialState}>
-            <BrowserRouter>
+            <BrowserRouter basename="/React-shop.git/">
             <Layout>
                 <Routes>
                     <Route path="/" element={<Home />} />
